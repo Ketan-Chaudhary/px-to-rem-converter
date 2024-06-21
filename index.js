a87dd65311ccea765b26e7059e4e2ea6b835db96
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-06-21 11:31:51 +0530

    version 1.0.0

---
+++ index.js
@@ -0,0 +1,38 @@
+const { exec } = require("child_process");
+const fs = require("fs");
+
+const inputFile = process.argv[2];
+const rootValue = parseFloat(process.argv[3]) || 16;
+
+if (!inputFile) {
+  console.error("Usage: node index.js <input-file> [root-value]");
+  process.exit(1);
+}
+
+// Update the root value dynamically in postcss.config.js
+const configContent = `
+module.exports = {
+    plugins: {
+        'postcss-pxtorem': {
+            rootValue: ${rootValue},
+            unitPrecision: 5,
+            propList: ['*'],
+            selectorBlackList: [],
+            replace: true,
+            mediaQuery: false,
+            minPixelValue: 0
+        }
+    }
+};
+`;
+fs.writeFileSync("postcss.config.js", configContent);
+
+// Run the postcss-cli command to process the CSS file
+exec(`npx postcss ${inputFile} --replace`, (err, stdout, stderr) => {
+  if (err) {
+    console.error(`Error: ${stderr}`);
+    process.exit(1);
+  } else {
+    console.log(`Conversion complete! File ${inputFile} has been updated.`);
+  }
+});
